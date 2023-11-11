da6f956f290cc568295a0a88299f045860a1bf44
kkimhyorr <hyorie03@naver.com>
2023-11-11 17:15:33 +0900

    feat: formik을 이용하여 register와 login 초기구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,36 @@
+import { Formik, Field, Form, FormikHelpers } from "formik";
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
-  return <div></div>;
+  return (
+    <>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={(
+          values: LoginForm,
+          { setSubmitting }: FormikHelpers<LoginForm>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form>
+          <Field id="email" name="email" placeholder="email"></Field>
+          <Field id="password" name="password" placeholder="password"></Field>
+          <button type="submit">login</button>
+        </Form>
+      </Formik>
+    </>
+  );
 };
 
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,39 @@
+import { Formik, Field, Form, FormikHelpers } from "formik";
+
+interface RegisterForm {
+  email: string;
+  password: string;
+  username: string;
+}
+
 const Register = () => {
-  return <div></div>;
+  return (
+    <>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          username: "",
+        }}
+        onSubmit={(
+          values: RegisterForm,
+          { setSubmitting }: FormikHelpers<RegisterForm>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form>
+          <Field id="email" name="email" placeholder="email"></Field>
+          <Field id="password" name="password" placeholder="password"></Field>
+          <Field id="username" name="username" placeholder="username"></Field>
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
+    </>
+  );
 };
 
 export default Register;
